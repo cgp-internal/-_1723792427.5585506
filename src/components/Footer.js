5fdb176b21955e878cfc1f39b5c905358d2741b4
cgp-client <code.gen.pro.system@gmail.com>
2024-08-16 10:13:59 +0300

    Adding src/components/Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p>&copy; 2023 My App</p>
+    </footer>
+  );
+}
+
+export { Footer };
+export default Footer;
