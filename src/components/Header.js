53cb85b98ee52aeb6264386b8deb9968f4361aa8
cgp-client <code.gen.pro.system@gmail.com>
2024-08-16 10:14:00 +0300

    Adding src/components/Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#" target="_blank" rel="noreferrer">Home</a></li>
+          <li><a href="#" target="_blank" rel="noreferrer">About</a></li>
+          <li><a href="#" target="_blank" rel="noreferrer">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export { Header as default, Header };
